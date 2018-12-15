694e21627fb0a38e66a3b040bc8cc3bd2e289220
Konstantin Tretyakov <kt@ut.ee>
2018-12-15 12:16:40 +0100

    Nicer skompile API

--- skompiler/api.py
+++ skompiler/api.py
@@ -5,29 +5,81 @@ Wraps around the intricacies of the various toskast/fromskast pieces.
 from .toskast.sklearn import translate as from_sklearn
 
 
-def skompile(model_or_method, inputs='x', method_name=None):
+def skompile(*args, inputs=None):
     """
     Creates a SKAST expression from a given bound method of a fitted SKLearn model.
     A shorthand notation for SKompiledModel(method, inputs)
 
     Args:
 
-        model_or_method:  Either a bound method of a trained model which you need to compile.
-                          (e.g. model.predict_proba), or the model object (in which case you need to
-                          provide the method name in a separate parameter)
+        args:  Either a bound method of a trained model (e.g. skompile(model.predict_proba)),
+               OR two arguments - a model and a method name (e.g. skompile(model, 'predict_proba')
+               (which may be necessary for some models where the first option cannot be used due to metaclasses)
         
-        inputs:  A string or a list of strings, denoting the input variables to your model.
+        inputs:  A string or a list of strings, or a SKAST node or a list of SKAST nodes,
+                 denoting the input variable(s) to your model.
                  A single string corresponds to a vector variable (which will be indexed to access
                  the components). A list of strings corresponds to a vector with separately named components.
-        
-        method_name:     If the first argument is a model object, specify the method name to generate code for.
+               You may pass the inputs as a non-keyword argument as well (the last one in *args)
+               If not specified, the default value of 'x' is used.
     
     Returns:
         An instance of SKompiledModel.
 
     Examples:
+        >>> from sklearn.datasets import load_iris
+        >>> from sklearn.linear_model import LogisticRegression
+        >>> X, y = load_iris(True)
+        >>> m = LogisticRegression().fit(X, y)
+        >>> print(skompile(m.predict))
+        argmax((([[...]] m@v x) + [ ...]))
+        >>> print(skompile(m, 'predict'))
+        argmax((([[...]] m@v x) + [ ...]))
+        >>> print(skompile(m.predict, 'y'))
+        argmax((([[...]] m@v y) + [ ...]))
+        >>> print(skompile(m.predict, ['x','y','z','w']))
+        argmax((([[...]] m@v [x, y, z, w]) + [ ...]))
+        >>> print(skompile(m, 'predict', 'y'))
+        argmax((([[...]] m@v y) + [ ...]))
+        >>> print(skompile(m, 'predict', ['x','y','z','w']))
+        argmax((([[...]] m@v [x, y, z, w]) + [ ...]))
+        >>> from skompiler.ast import VectorIdentifier, Identifier
+        >>> print(skompile(m, 'predict', VectorIdentifier('y', 4)))
+        argmax((([[...]] m@v y) + [ ...]))
+        >>> print(skompile(m, 'predict', map(Identifier, ['x','y','z','w'])))
+        argmax((([[...]] m@v [x, y, z, w]) + [ ...]))
+        >>> from sklearn.pipeline import Pipeline
+        >>> p = Pipeline([('1', m)])
+        >>> skompile(p.predict)
+        Traceback (most recent call last):
+        ...
+        ValueError: The bound method ... Please, use the skompile(m, 'predict') syntax instead.
     """
-    if method_name is None:
-        method_name = model_or_method.__name__
-        model_or_method = model_or_method.__self__
-    return from_sklearn(model_or_method, inputs=inputs, method=method_name)
+
+    if len(args) > 3:
+        raise ValueError("Too many arguments")
+    elif not args:
+        raise ValueError("Invalid arguments")
+    elif len(args) == 3:
+        if inputs is not None:
+            raise ValueError("Too many arguments")
+        model, method, inputs = args
+    elif len(args) == 2:
+        if hasattr(args[0], '__call__'):
+            model, method = _get_model_and_method(args[0])
+            inputs = args[1]
+        else:
+            model, method = args
+    else:
+        model, method = _get_model_and_method(args[0])
+    if not inputs:
+        inputs = 'x'
+    return from_sklearn(model, inputs=inputs, method=method)
+
+def _get_model_and_method(obj):
+    if not hasattr(obj, '__self__'):
+        raise ValueError("The bound method object was probably mangled by "
+                         "SKLearn's metaclasses and cannot be passed to skompile as skompile(m.predict). "
+                         "Please, use the skompile(m, 'predict') syntax instead.")
+    return obj.__self__, obj.__name__
+
