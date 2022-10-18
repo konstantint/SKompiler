03d7a813cb68a2a52cf272674c181d905f392264
Matthew Epland <matthewepland@gmail.com>
2022-10-18 19:40:17 -0400

    Let trees that use less than all of the avaliable features run as normal

--- skompiler/toskast/sklearn/tree/base.py
+++ skompiler/toskast/sklearn/tree/base.py
@@ -80,8 +80,8 @@ class TreeWalker:
         
         self.tree = tree
         self.features = features
-        if tree.n_features != len(features):
-            raise ValueError("Incorrect number of features provided")
+        if len(self.features) < tree.n_features:
+            raise ValueError(f"Incorrect number of features provided. Expect {tree.n_features} but have {self.features}")
         if node_values is None:
             self.values = tree.value[:, 0]
             if self.values.shape[1] == 1:
@@ -102,7 +102,7 @@ class TreeWalker:
                 return const(self.values[node_id])
         else:
             ft = self.tree.feature[node_id]
-            if ft < 0 or ft >= self.tree.n_features:
+            if ft < 0 or ft >= len(self.features):
                 raise ValueError("Invalid feature value for node {0}".format(node_id))
             return iif(self.features[ft] <= const(self.tree.threshold[node_id].item()),
                        self.walk(self.tree.children_left[node_id]),
